79a417a33a982268dd20d884be81eea69a3ef5da
hiroyuki517 <139934552+hiroyuki517@users.noreply.github.com>
2023-08-31 13:46:02 +0900

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(function(){
+
+    $("#thumbs img").click(function(){
+        let indexNum = $(this).parent().index();
+
+        //モーダルの表示とコンテンツのリセット
+        $("#modal-wrapper").fadeIn(300);
+        $("#left-side li").hide();
+        $("#title li").hide();
+        $("#text li").hide();
+
+        //関数化
+        $("#main-image").children().eq(indexNum).addClass("vision");
+        $("#title").children().eq(indexNum).addClass("vision");
+        $("#text").children().eq(indexNum).addClass("vision");
+
+        $("#main-image .vision").fadeIn(500);
+        $("#right-side .vision").slideDown(1000);
+    });
+
+    $("#modal-wrapper").click(function(){
+        $(this).fadeOut(300);
+        $(".vision").removeClass("vision");
+    });
+
+});
